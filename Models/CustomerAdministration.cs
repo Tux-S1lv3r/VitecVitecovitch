2058976abc40343a1b2ccfc57d14c429d1cf8ac6
Faiz <faiz0095@edu.eal.dk>
2019-09-17 12:03:50 +0200

    Add project files.

---
+++ Models/CustomerAdministration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VitecVitecovitch.Models
+{
+    public class CustomerAdministration
+    {
+        public int ID { get; set; }
+        public string CustomerName { get; set; }
+        public int TelephoneNumber { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        public int CPR { get; set; }
+        public string Subscription { get; set; }
+        public int Price { get; set; }
+    }
+}
+ec
